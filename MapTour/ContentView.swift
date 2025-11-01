3745caf811b0aa0d4d4ae06972dcc730d6ba82ee
Alikhan Zhanibek <bayalikhan@bk.ru>
2025-11-01 14:51:29 -0500

    Initial Commit

---
+++ MapTour/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MapTour
+//
+//  Created by Alikhan Zhanibek on 2025-11-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MapTour/MapTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapTourApp.swift
+//  MapTour
+//
+//  Created by Alikhan Zhanibek on 2025-11-01.
+//
+
+import SwiftUI
+
+@main
+struct MapTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
