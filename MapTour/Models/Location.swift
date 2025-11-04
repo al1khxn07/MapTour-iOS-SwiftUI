a75b5d611ed2e0b8a05f81567f96186e8512ff4b
Alikhan Zhanibek <bayalikhan@bk.ru>
2025-11-04 17:23:33 -0600

    Initial commit

---
+++ MapTour/Models/Location.swift
@@ -0,0 +1,8 @@
+//
+//  Location.swift
+//  MapTour
+//
+//  Created by Alikhan Zhanibek on 2025-11-01.
+//
+
+import Foundation
---
+++ MapTour/Views/LocationsView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationsView.swift
+//  MapTour
+//
+//  Created by Alikhan Zhanibek on 2025-11-01.
+//
+
+import SwiftUI
+
+struct LocationsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationsView()
+}
